69ba43a0a0286951c378b43cc04160f2e4017581
sobralAllan <allan.sobraldasilva@gmail.com>
2022-06-28 10:29:53 -0300

    Final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 import this
 import operacoes
-
+this.codigo = 0
 this.nome = ""
 this.telefone = ""
 this.nascimento = ""
@@ -18,6 +18,33 @@ def cadastrar():
         this.nascimento = request.form['tNovaDataDeNascimento']
         this.dados = operacoes.inserir(this.nome, this.telefone, this.nascimento)
     return render_template('index.html', titulo='Página Principal', resultado=this.dados)
+
+@calculadora.route('/atualizar.html', methods=['GET','POST'])
+def atualizar():
+    if request.method == 'POST':
+       this.codigo = request.form.get("tCodigo")
+       this.nome   = request.form.get("tNovoNome")
+       this.telefone = request.form.get("tNovoTelefone")
+       this.nascimento = request.form.get("tNovaDataDeNascimento")
+       #Métodos para atualização dos dados
+       this.dados  = operacoes.atualizar(this.codigo, 'nome', this.nome)
+       this.dados  = operacoes.atualizar(this.codigo, 'telefone', this.telefone)
+       this.dados  = operacoes.atualizar(this.codigo, 'DataDeNascimento', this.nascimento)
+    return render_template('atualizar.html', titulo='Atualizar Dados', resultado=this.dados)
+
+@calculadora.route('/excluir.html', methods=['GET','POST'])
+def excluir():
+    if request.method == 'POST':
+        this.codigo = request.form.get("tCodigo")
+        this.dados = operacoes.excluir(this.codigo)
+    return render_template('excluir.html', titulo='Excluir Dados', resultado=this.dados)
+
+@calculadora.route('/consultar.html', methods=['GET', 'POST'])
+def consultarCodigo():
+    if request.method == 'POST':
+        this.codigo = request.form.get("tCodigo")
+        this.dados = operacoes.consultar(this.codigo)
+    return render_template('consultar.html', titulo='Consultar', resultado = this.dados)
 
 if __name__ == '__main__':
     calculadora.run(debug=True, port=5000)
--- operacoes.py
+++ operacoes.py
@@ -1,6 +1,7 @@
 import conexaoBD
 import mysql.connector
-
+import this
+this.msg = ""
 db_connection = conexaoBD.conexao()
 con = db_connection.cursor()
 
@@ -9,6 +10,36 @@ def inserir(nome, telefone, dataNascimento):
         sql = "Insert into pessoa(codigo, nome, telefone, dataDeNascimento) values('','{}','{}','{}')".format(nome, telefone, dataNascimento)
         con.execute(sql)
         db_connection.commit()
-        return con.rowcount, "Inserido!"
+        return "{} Inserido!".format(con.rowcount)
+    except Exception as erro:
+        return erro
+
+def consultar(codigo):
+    try:
+        sql = "Select * from pessoa where codigo = '{}'".format(codigo)
+        con.execute(sql)
+
+        for(codigo, nome, telefone, dataDeNascimento) in con:
+            this.msg = "Código: '{}', Nome: '{}', Telefone: '{}', Data de Nascimento: '{}'".format(codigo, nome, telefone, dataDeNascimento)
+        return this.msg
+
+    except Exception as erro:
+        return erro
+
+def atualizar(codigo, campo, novoDado):
+    try:
+        sql = "Update pessoa set {} = '{}' where codigo = '{}'".format(campo, novoDado, codigo)
+        con.execute(sql)
+        db_connection.commit()
+        return "{} Atualizado!".format(con.rowcount)
+    except Exception as erro:
+        return erro
+
+def excluir(codigo):
+    try:
+        sql = "delete from pessoa where codigo = '{}'".format(codigo)
+        con.execute(sql)
+        db_connection.commit()
+        return "{} Excluído!".format(con.rowcount)
     except Exception as erro:
         return erro
